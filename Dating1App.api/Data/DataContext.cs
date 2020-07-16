d083553158e2a3b64dc42966f629d3a0a12f13c2
kunalheda <kunalheda4@gmail.com>
2020-07-16 20:21:45 +0530

    initial commit

---
+++ Dating1App.api/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Dating1App.api.Modules;
+namespace Dating1App.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Value> val   { get; set; }
+    }
+}
---
+++ Dating1App.api/Modules/Value.cs
@@ -0,0 +1,8 @@
+namespace Dating1App.api.Modules
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
