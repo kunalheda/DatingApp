a198c4b2f685931033436f991ae937050955c46c
kunalheda <kunalheda4@gmail.com>
2020-08-21 21:41:26 +0530

    End of sec 5

--- Dating1App.api/Controllers/AuthController.cs
+++ Dating1App.api/Controllers/AuthController.cs
@@ -48,6 +48,7 @@ namespace Dating1App.api.Controllers
 
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+             
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
             if (userFromRepo == null)
             {
